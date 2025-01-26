313322f47fb6755b7d658d09f4fde2d36b29d243
TecVit <vitorfreelancer021@gmail.com>
2025-01-26 20:32:22 -0300

    update - parte 3

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: ["https://olhaprofessor.vercel.app/", "http://localhost:3000"],
+    originWhitelist: [],
     requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+    removeHeaders: ['cookie', 'cookie2'],
+    setHeaders: {
+        'Access-Control-Allow-Origin': '*', // Pode especificar um domínio específico aqui, como 'https://seu-dominio.com'
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // Adicione os métodos que você deseja permitir
+        'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type', // Adicione os cabeçalhos que você deseja permitir
+    }
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
