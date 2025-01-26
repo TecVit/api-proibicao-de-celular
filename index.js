8cf3eca7c623ada6f7b8b5ce6a646887a31537c8
TecVit <vitorfreelancer021@gmail.com>
2025-01-26 20:17:16 -0300

    update - parte 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [],
+    originWhitelist: ["https://olhaprofessor.vercel.app/", "http://localhost:3000"],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
